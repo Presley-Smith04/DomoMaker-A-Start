f057fed95bb101628d1f954b5d80196214f4fa46
Presley-Smith04 <presleysmith04@gmail.com>
2025-04-09 15:19:03 -0400

    domo C

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
@@ -10,41 +12,60 @@ const session = require('express-session');
 const RedisStore = require('connect-redis').default;
 const redis = require('redis');
 
+
 const router = require('./router.js');
 
+
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
   if (err) {
-    console.log('couldnt connect');
+    console.log('Could not connect to database');
     throw err;
   }
 });
 
-const app = express();
 
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const redisClient = redis.createClient({
+  username: 'default',
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: 'redis-16730.c1.us-central1-2.gce.redns.redis-cloud.com',
+    port: 16730,
+  },
+});
 
-app.use(session({
-  key: 'sessionid',
-  secret: 'Domo-Arigato',
-  resave: false,
-  saveUninitialized: false,
-}));
 
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.connect().then(() => {
+  const app = express();
+  app.use(helmet());
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
-router(app);
+  app.use(session({
+    key: 'sessionid',
+    store: new RedisStore({ client: redisClient }),
+    secret: 'Domo Arigato',
+    resave: false,
+    saveUninitialized: false,
+  }));
 
-app.listen(port, (err) => {
-  if (err) { throw err; }
-  console.log(`Listening on port ${port}`);
+  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+  app.set('view engine', 'handlebars');
+  app.set('views', `${__dirname}/../views`);
+
+  router(app);
+
+  app.listen(port, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`Listening on port ${port}`);
+  });
 });
